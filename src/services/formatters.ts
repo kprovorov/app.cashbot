652f93318a9fea62e14a81929b99df3fa13b466f
Kirill Provorov <kirill@provorov.dev>
2022-09-11 18:29:23 +0200

    currency format

--- src/services/formatters.ts
+++ src/services/formatters.ts
@@ -2,8 +2,9 @@ export function currencyFormat(value: number, currency: string): string {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency,
-    minimumFractionDigits: 2, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    maximumFractionDigits: 2, // (causes 2500.99 to be printed as $2,501)
+    minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    currencyDisplay: "narrowSymbol",
   });
 
   return formatter.format(value / 10000);
