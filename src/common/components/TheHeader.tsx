e1f9160dcd61b82ce203aaba8629532868ed8c97
Kirill Provorov <kirill@provorov.dev>
2023-02-21 20:13:24 +0100

    install tailwind

--- src/common/components/TheHeader.tsx
+++ src/common/components/TheHeader.tsx
@@ -9,26 +9,24 @@ export default function TheHeader({
   onCreated,
 }: PropsWithChildren<{ onCreated: () => void }>) {
   return (
-    <>
-      <Navbar bg="dark" expand={true} fixed="top" variant="dark">
-        <Container fluid className="d-flex align-items-center">
-          <div>
-            <Navbar.Brand href="/">
-              <img height="20" src="logo.svg" alt="cashbot" />
-            </Navbar.Brand>
-            <CreateTransferButton onCreated={onCreated} />
-            <CreatePaymentButton onCreated={onCreated} />
-          </div>
-          <Button
-            size="sm"
-            as="a"
-            href={`${import.meta.env.VITE_ID_APP_URL}/login`}
-            variant="dark"
-          >
-            Login
-          </Button>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="dark" expand={true} fixed="top" variant="dark">
+      <Container fluid className="d-flex align-items-center">
+        <div className="tw-flex">
+          <Navbar.Brand href="/">
+            <img className="tw-h-5" src="logo.svg" alt="cashbot" />
+          </Navbar.Brand>
+          <CreateTransferButton onCreated={onCreated} />
+          <CreatePaymentButton onCreated={onCreated} />
+        </div>
+        <Button
+          size="sm"
+          as="a"
+          href={`${import.meta.env.VITE_ID_APP_URL}/login`}
+          variant="dark"
+        >
+          Login
+        </Button>
+      </Container>
+    </Navbar>
   );
 }
