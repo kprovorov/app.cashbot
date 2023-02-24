7dc35c4f36e6cd341521cf82d137433572f0e4cb
Kirill Provorov <kirill@provorov.dev>
2023-02-24 17:16:31 +0100

    header link

--- src/common/components/HeaderLink.tsx
+++ src/common/components/HeaderLink.tsx
@@ -1,3 +1,3 @@
 import tw from "tailwind-styled-components";
 
-export default tw.a`hover:tw-bg-slate-700 tw-p-2 tw-rounded tw-transition tw-duration-200 tw-ease-in-out`;
+export default tw.a`tw-px-4 tw-py-2 hover:tw-bg-slate-700 tw-p-2 tw-rounded tw-transition tw-duration-200 tw-ease-in-out`;
