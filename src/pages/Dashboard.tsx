c737b2ebb453f636d6a456ae47aea38bf00b4dc6
Kirill Provorov <kirill@provorov.dev>
2023-02-24 17:13:49 +0100

    refresh button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -85,17 +85,22 @@ function Dashboard() {
               className="hover:tw-bg-slate-900/5"
               onClick={fetchDashboard}
             >
-              {loading ? (
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                  aria-hidden="true"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={`tw-w-6 tw-h-6 ${
+                  loading ? "tw-animate-spin" : null
+                }`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
                 />
-              ) : (
-                "Refresh"
-              )}
+              </svg>
             </Button>
           </div>
         </div>
