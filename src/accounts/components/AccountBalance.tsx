c2c33c45cea3518a78fba86ddddff528f3a99025
Kirill Provorov <kirill@provorov.dev>
2023-02-25 16:38:03 +0100

    AccountBalance

--- src/accounts/components/AccountBalance.tsx
+++ src/accounts/components/AccountBalance.tsx
@@ -1,7 +1,14 @@
-import React, { PropsWithChildren, useState } from "react";
-import AccountUpdateBalanceForm from "./AccountUpdateBalanceForm";
-import { currencyFormat } from "../../services/formatters";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  PropsWithChildren,
+  useRef,
+  useState,
+} from "react";
+import { updateAccount } from "../../api/accounts";
+import Input from "../../common/components/ui/forms/Input";
 import Account from "../../interfaces/Account";
+import { currencyFormat } from "../../services/formatters";
 
 export default function AccountBalance({
   account,
@@ -10,23 +17,44 @@ export default function AccountBalance({
   account: Account;
   onUpdated: () => void;
 }>) {
-  const [editing, setEditing] = useState<boolean>(false);
+  const formRef = useRef<HTMLFormElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [balance, setBalance] = useState<number>(account.balance);
+
+  const update = async () => {
+    if (balance !== account.balance) {
+      await updateAccount(account.id, { ...account, balance });
+      onUpdated();
+    }
+  };
+
+  const reset = () => {
+    setBalance(account.balance);
+  };
+
+  const submit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await update();
+  };
 
   return (
-    <div>
-      {editing ? (
-        <AccountUpdateBalanceForm
-          account={account}
-          onUpdated={() => {
-            setEditing(false);
-            onUpdated();
-          }}
-        />
-      ) : (
-        <div onClick={() => setEditing(true)}>
-          {currencyFormat(account.balance, account.currency)}
-        </div>
-      )}
-    </div>
+    <form onSubmit={submit} ref={formRef}>
+      <Input
+        ref={inputRef}
+        onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>): void => {
+          if (e.key === "Escape") {
+            reset();
+          }
+        }}
+        className="border-transparent bg-transparent w-24 text-right"
+        type="text"
+        placeholder="Balance"
+        value={currencyFormat(balance, account.currency)}
+        onChange={(e): void => {
+          setBalance(Number(e.target.value.replace(/\D/g, "")) * 10000);
+        }}
+        onBlur={update}
+      />
+    </form>
   );
 }
--- src/common/components/ui/forms/Input.tsx
+++ src/common/components/ui/forms/Input.tsx
@@ -1,3 +1,3 @@
 import tw from "tailwind-styled-components";
 
-export default tw.input`w-full border border-gray-300 rounded py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline`;
+export default tw.input`w-full border border-gray-300 rounded py-2 px-3 text-gray-900 leading-tight`;
