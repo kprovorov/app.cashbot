0744876eea3e500deb0bee2a3c1e58c4d527c119
Kirill Provorov <kirill@provorov.dev>
2023-03-24 10:51:13 +0100

    install sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "./index.css";
 import "swiper/css";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
