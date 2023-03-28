b9472b5ed238fba9adebd4300490d28122025fd5
Kirill Provorov <kirill@provorov.dev>
2023-03-28 09:30:59 +0200

    Redesign create form (#24)

--- src/payments/components/CreatePaymentForm.tsx
+++ src/payments/components/CreatePaymentForm.tsx
@@ -13,6 +13,7 @@ import Checkbox from "../../common/components/ui/forms/Checkbox";
 import PrimaryButton from "../../common/components/ui/buttons/PrimaryButton";
 import SecondaryButton from "../../common/components/ui/buttons/SecondaryButton";
 import { Tab } from "@headlessui/react";
+import Button from "../../common/components/ui/buttons/Button";
 
 export default function CreatePaymentForm({
   onCreated,
@@ -83,8 +84,8 @@ export default function CreatePaymentForm({
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-12">
+      <div className="flex flex-col gap-4">
+        <div>
           <div className="bg-slate-100 p-2 rounded-lg">
             <Tab.Group
               defaultIndex={Object.values(PaymentType).indexOf(paymentType)}
@@ -103,7 +104,7 @@ export default function CreatePaymentForm({
             </Tab.Group>
           </div>
         </div>
-        <div className="col-span-12 flex flex-row items-top gap-2 text-slate-400">
+        <div className="flex flex-row items-top gap-2 text-slate-400">
           <div className="w-8 h-8">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -123,145 +124,180 @@ export default function CreatePaymentForm({
           <div className="flex items-center">{descriptions[paymentType]}</div>
         </div>
 
-        <div className="col-span-6 sm:col-span-3">
-          <Label htmlFor="date">Date</Label>
-          <Input
-            type="date"
-            id="date"
-            name="date"
-            value={moment(formik.values.date).format("YYYY-MM-DD")}
-            onChange={formik.handleChange}
-            $invalid={!!formik.errors.date}
-          />
-          <InputError>{formik.errors.date}</InputError>
-        </div>
-        <div className="col-span-6 sm:col-span-3">
-          <Label htmlFor="repeat_unit">Repeat Unit</Label>
-          <Input
-            $as="select"
-            id="repeat_unit"
-            name="repeat_unit"
-            value={formik.values.repeat_unit}
-            onChange={formik.handleChange}
-            $invalid={!!formik.errors.repeat_unit}
-          >
-            {Object.keys(RepeatUnit).map((key) => (
-              <option key={key} value={key}>
-                {key}
-              </option>
-            ))}
-          </Input>
-          <InputError>{formik.errors.repeat_unit}</InputError>
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+          <div>
+            <Label htmlFor="date">Date</Label>
+            <Input
+              type="date"
+              id="date"
+              name="date"
+              value={moment(formik.values.date).format("YYYY-MM-DD")}
+              onChange={formik.handleChange}
+              $invalid={!!formik.errors.date}
+            />
+            <InputError>{formik.errors.date}</InputError>
+          </div>
+          <div>
+            <Label htmlFor="repeat_unit">Repeat Unit</Label>
+            <Input
+              $as="select"
+              id="repeat_unit"
+              name="repeat_unit"
+              value={formik.values.repeat_unit}
+              onChange={formik.handleChange}
+              $invalid={!!formik.errors.repeat_unit}
+            >
+              {Object.keys(RepeatUnit).map((key) => (
+                <option key={key} value={key}>
+                  {key}
+                </option>
+              ))}
+            </Input>
+            <InputError>{formik.errors.repeat_unit}</InputError>
+          </div>
+          <div>
+            <Label htmlFor="repeat_interval">Repeat interval</Label>
+            <Input
+              disabled={formik.values.repeat_unit === RepeatUnit.NONE}
+              type="number"
+              id="repeat_interval"
+              name="repeat_interval"
+              value={formik.values.repeat_interval}
+              onChange={formik.handleChange}
+              $invalid={!!formik.errors.repeat_interval}
+            />
+            <InputError>{formik.errors.repeat_interval}</InputError>
+          </div>
+          <div>
+            <Label htmlFor="repeat_ends_on">Repeat Ends</Label>
+            <Input
+              disabled={formik.values.repeat_unit === RepeatUnit.NONE}
+              type="date"
+              id="repeat_ends_on"
+              name="repeat_ends_on"
+              value={moment(formik.values.repeat_ends_on).format("YYYY-MM-DD")}
+              onChange={formik.handleChange}
+              $invalid={!!formik.errors.repeat_ends_on}
+            />
+            <InputError>{formik.errors.repeat_ends_on}</InputError>
+          </div>
         </div>
 
-        <div className="col-span-6 sm:col-span-3">
-          <Label htmlFor="repeat_interval">Repeat interval</Label>
-          <Input
-            disabled={formik.values.repeat_unit === RepeatUnit.NONE}
-            type="number"
-            id="repeat_interval"
-            name="repeat_interval"
-            value={formik.values.repeat_interval}
-            onChange={formik.handleChange}
-            $invalid={!!formik.errors.repeat_interval}
-          />
-          <InputError>{formik.errors.repeat_interval}</InputError>
-        </div>
+        <div className="flex flex-col gap-4 items-center w-full">
+          <div className="flex flex-col items-center bg-slate-100 p-4 rounded-lg w-full">
+            <Input
+              type="text"
+              className="text-center text-5xl border-none focus:outline-none focus:ring-0 bg-transparent"
+              id="amount"
+              name="amount"
+              value={formik.values.amount * 1}
+              onChange={formik.handleChange}
+              $invalid={!!formik.errors.amount}
+              autoComplete="off"
+            />
 
-        <div className="col-span-6 sm:col-span-3">
-          <Label htmlFor="repeat_ends_on">Repeat Ends</Label>
-          <Input
-            disabled={formik.values.repeat_unit === RepeatUnit.NONE}
-            type="date"
-            id="repeat_ends_on"
-            name="repeat_ends_on"
-            value={moment(formik.values.repeat_ends_on).format("YYYY-MM-DD")}
-            onChange={formik.handleChange}
-            $invalid={!!formik.errors.repeat_ends_on}
-          />
-          <InputError>{formik.errors.repeat_ends_on}</InputError>
-        </div>
+            <SecondaryButton
+              type="button"
+              className="border-none hover:bg-slate-200"
+              onClick={() => {
+                const currencies = Object.values(Currency);
 
-        <div className="col-span-6 sm:col-span-3">
-          <Label htmlFor="amount">Amount</Label>
-          <Input
-            type="text"
-            id="amount"
-            name="amount"
-            value={formik.values.amount}
-            onChange={formik.handleChange}
-            $invalid={!!formik.errors.amount}
-          />
+                const index = currencies.indexOf(formik.values.currency);
+
+                formik.setFieldValue(
+                  "currency",
+                  currencies[index + 1 >= currencies.length ? 0 : index + 1]
+                );
+              }}
+            >
+              {formik.values.currency}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M13.2 2.24a.75.75 0 00.04 1.06l2.1 1.95H6.75a.75.75 0 000 1.5h8.59l-2.1 1.95a.75.75 0 101.02 1.1l3.5-3.25a.75.75 0 000-1.1l-3.5-3.25a.75.75 0 00-1.06.04zm-6.4 8a.75.75 0 00-1.06-.04l-3.5 3.25a.75.75 0 000 1.1l3.5 3.25a.75.75 0 101.02-1.1l-2.1-1.95h8.59a.75.75 0 000-1.5H4.66l2.1-1.95a.75.75 0 00.04-1.06z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </SecondaryButton>
+          </div>
           <InputError>{formik.errors.amount}</InputError>
         </div>
-        <div className="col-span-6 sm:col-span-3">
-          <Label htmlFor="currency">Currency</Label>
-          <Input
-            $as="select"
-            id="currency"
-            name="currency"
-            value={formik.values.currency}
-            onChange={formik.handleChange}
-            $invalid={!!formik.errors.currency}
-          >
-            {Object.keys(Currency).map((key) => (
-              <option key={key} value={key}>
-                {key}
-              </option>
-            ))}
-          </Input>
-          <InputError>{formik.errors.currency}</InputError>
+
+        <div className="flex flex-row gap-4 items-end justify-center">
+          {[
+            PaymentType.EXPENSE,
+            PaymentType.TRANSFER,
+            PaymentType.BUDGET,
+          ].includes(paymentType) && (
+            <div className="flex-grow">
+              <Label htmlFor="account_from_id">Account From</Label>
+              <Input
+                $as="select"
+                id="account_from_id"
+                name="account_from_id"
+                value={formik.values.account_from_id}
+                onChange={formik.handleChange}
+                $invalid={!!formik.errors.account_from_id}
+              >
+                <option value={""}>Please select...</option>
+                {accounts?.map((account) => (
+                  <option key={account.id} value={account.id}>
+                    {account.name} ({account.currency})
+                  </option>
+                ))}
+              </Input>
+              <InputError>{formik.errors.account_from_id}</InputError>
+            </div>
+          )}
+
+          {paymentType === PaymentType.TRANSFER && (
+            <div className="pb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </div>
+          )}
+
+          {[PaymentType.INCOME, PaymentType.TRANSFER].includes(paymentType) && (
+            <div className="flex-grow">
+              <Label htmlFor="account_to_id">Account To</Label>
+              <Input
+                $as="select"
+                id="account_to_id"
+                name="account_to_id"
+                value={formik.values.account_to_id}
+                onChange={formik.handleChange}
+                $invalid={!!formik.errors.account_to_id}
+              >
+                <option value={""}>Please select...</option>
+                {accounts?.map((account) => (
+                  <option key={account.id} value={account.id}>
+                    {account.name} ({account.currency})
+                  </option>
+                ))}
+              </Input>
+              <InputError>{formik.errors.account_to_id}</InputError>
+            </div>
+          )}
         </div>
 
-        {[
-          PaymentType.EXPENSE,
-          PaymentType.TRANSFER,
-          PaymentType.BUDGET,
-        ].includes(paymentType) && (
-          <div className="col-span-6 sm:col-span-3">
-            <Label htmlFor="account_from_id">Account From</Label>
-            <Input
-              $as="select"
-              id="account_from_id"
-              name="account_from_id"
-              value={formik.values.account_from_id}
-              onChange={formik.handleChange}
-              $invalid={!!formik.errors.account_from_id}
-            >
-              <option value={""}>Please select...</option>
-              {accounts?.map((account) => (
-                <option key={account.id} value={account.id}>
-                  {account.name} ({account.currency})
-                </option>
-              ))}
-            </Input>
-            <InputError>{formik.errors.account_from_id}</InputError>
-          </div>
-        )}
-
-        {[PaymentType.INCOME, PaymentType.TRANSFER].includes(paymentType) && (
-          <div className="col-span-6 sm:col-span-3">
-            <Label htmlFor="account_to_id">Account To</Label>
-            <Input
-              $as="select"
-              id="account_to_id"
-              name="account_to_id"
-              value={formik.values.account_to_id}
-              onChange={formik.handleChange}
-              $invalid={!!formik.errors.account_to_id}
-            >
-              <option value={""}>Please select...</option>
-              {accounts?.map((account) => (
-                <option key={account.id} value={account.id}>
-                  {account.name} ({account.currency})
-                </option>
-              ))}
-            </Input>
-            <InputError>{formik.errors.account_to_id}</InputError>
-          </div>
-        )}
-
-        <div className="col-span-12">
+        <div>
           <Label htmlFor="description">Description</Label>
           <Input
             type="text"
@@ -273,8 +309,10 @@ export default function CreatePaymentForm({
           />
           <InputError>{formik.errors.description}</InputError>
         </div>
-        <div className="col-span-12 flex justify-end gap-3">
-          <SecondaryButton onClick={onCancel}>Close</SecondaryButton>
+        <div className=" flex justify-end gap-3">
+          <SecondaryButton type="button" onClick={onCancel}>
+            Close
+          </SecondaryButton>
           <PrimaryButton type="submit">Save changes</PrimaryButton>
         </div>
       </div>
