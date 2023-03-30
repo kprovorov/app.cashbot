2360a8cbe87eb8bcf7b5e47cb354456f5b6729ff
Kirill Provorov <kirill@provorov.dev>
2023-03-30 15:52:07 +0200

    Floating plus button (#26)

--- src/payments/components/CreatePaymentButton.tsx
+++ src/payments/components/CreatePaymentButton.tsx
@@ -10,7 +10,25 @@ export default function CreatePaymentButton() {
 
   return (
     <>
-      <HeaderButton onClick={handleShow}>Add Payment</HeaderButton>
+      <button
+        onClick={handleShow}
+        className="fixed bg-primary text-white p-5 z-10 right-10 bottom-10 rounded-3xl hover:bg-primary-darken shadow-lg shadow-primary/50"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+      </button>
       <CreatePaymentModal show={show} onClose={handleClose} />
     </>
   );
