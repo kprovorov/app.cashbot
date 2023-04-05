1f2f67570fbd6650be12b09947c80c5d5a876e4c
Kirill Provorov <kirill@provorov.dev>
2023-04-05 22:10:40 +0200

    header

--- src/common/components/TheHeader.tsx
+++ src/common/components/TheHeader.tsx
@@ -23,7 +23,7 @@ export default function TheHeader() {
   };
 
   return (
-    <div className="flex bg-gray-dark p-3 text-white justify-between items-center rounded-xl">
+    <div className="flex bg-gray-darkest p-3 text-white justify-between items-center rounded-xl shadow-lg">
       <div className="flex gap-3 items-center">
         <Link to="/" className="px-3">
           <img className="h-6" src="logo.svg" alt="cashbot" />
@@ -31,20 +31,16 @@ export default function TheHeader() {
         {user ? <CreatePaymentButton /> : null}
       </div>
       {user ? (
-        <div className="flex items-center gap-3">
+        <div className="flex items-center">
           <Button
             $as={Link}
             to="/profile"
             $size="sm"
-            className="font-medium hover:bg-gray-darkest/30 text-white"
+            className="font-medium hover:bg-black text-white"
           >
             {user.name}
           </Button>
-          <Button
-            $size="sm"
-            onClick={logout}
-            className="hover:bg-gray-darkest/30"
-          >
+          <Button $size="sm" onClick={logout} className="hover:bg-black">
             <ArrowRightOnRectangleIcon className="w-6 h-6 text-white" />
           </Button>
         </div>
